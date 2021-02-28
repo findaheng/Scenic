07fc6bb0cc8785346d911a358523f1d86619069d
Francis Indaheng <findaheng@berkeley.edu>
2021-02-28 13:58:07 -0800

    minor typo

--- examples/carla/Behavior_Prediction/__main__.py
+++ examples/carla/Behavior_Prediction/__main__.py
@@ -2,8 +2,8 @@ import argparse
 import subprocess
 
 parser = argparse.ArgumentParser()
-parser.addArgument('--type', '-t', type=str, default='intersection', help='Specify traffic type')
-parser.addArgument('--number', '-n', type=str, default='01', help='Specify scenario number in form ##')
+parser.add_argument('--type', '-t', type=str, default='intersection', help='Specify traffic type')
+parser.add_argument('--number', '-n', type=str, default='01', help='Specify scenario number in form ##')
 args = parser.parse_arg()
 
 path = f'{args.type}/{args.type}_{args.number}.scenic'
