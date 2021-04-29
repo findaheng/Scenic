27f6d8f1e0a91bdaf440a82ef6b3316ca9e643b1
Francis Indaheng <findaheng@berkeley.edu>
2021-04-28 22:27:55 -0700

    refactor

--- examples/carla/Behavior_Prediction/falsify.py
+++ examples/carla/Behavior_Prediction/falsify.py
@@ -15,27 +15,31 @@ from verifai.falsifier import generic_falsifier, generic_parallel_falsifier
 from verifai.monitor import multi_objective_monitor
 
 class ADE_FDE(multi_objective_monitor):
-    def __init__(self, model_path, timepoint=20, debug=False):
+    def __init__(self, model_path, thresholds=(2, 2), timepoint=20, worker_num=0, debug=False):
+        priority_graph = None
         self.model_path = model_path
         self.num_objectives = 2
-        self.timepoint = timepoint; assert timepoint >= 20, 'Must allow at least 20 timesteps of past trajectories!'
+        self.worker_num = worker_num
         self.debug = debug
-        priority_graph = None
+        self.thresholds = thresholds; assert len(thresholds) == self.num_objectives
+        self.timepoint = timepoint; assert timepoint >= 20, 'Must allow at least 20 timesteps of past trajectories!'
 
         def specification(traj):
-        	num_agents = len(traj[0])
+            num_agents = len(traj[0])
             hist_traj = traj[timepoint-20:timepoint]
             gt_traj = traj[timepoint:timepoint+15]
             gts = np.asarray([(tj[-1][0], tj[-1][1]) for tj in gt_traj])
             gt_len = len(gts)
+            threshADE, threshFDE = self.thresholds
 
             if self.debug:
+                print(f'ADE Threshold: {threshADE}, FDE Threshold: {threshFDE}')
                 plt.plot([gt[-1][0] for gt in traj], [gt[-1][1] for gt in traj], color='black')
                 plt.plot([gt[-1][0] for gt in hist_traj], [gt[-1][1] for gt in hist_traj], color='blue')
                 plt.plot([gt[-1][0] for gt in gt_traj], [gt[-1][1] for gt in gt_traj], color='yellow')
 
-            # process historical trajectory csv file
-            with open(f"{model_path}/dataset/test_obs/data/0.csv", 'w', newline='') as csvfile:
+            # Process historical trajectory CSV file
+            with open(f'{model_path}/dataset/test_obs/data/{self.worker_num}.csv', 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(['TIMESTAMP', 'TRACK_ID', 'OBJECT_TYPE', 'X', 'Y', 'CITY_NAME'])
                 track_id = '00000000-0000-0000-0000-000000000000'
@@ -50,24 +54,20 @@ class ADE_FDE(multi_objective_monitor):
                         writer.writerow([timestamp, track_id, obj_type, agent_pos[0], agent_pos[1], 'N/A'])
             csvfile.close()
 
-            # run behavior prediction model
+            # Run behavior prediction model
             os.chdir(model_path)
             subprocess.run(['python', 'preprocess_data.py', '-n', '1'])
             subprocess.run(['python', 'test.py', '-m', 'lanegcn', f'--weight={model_path}/36.000.ckpt', '--split=test', '--map_path=/home/carla_challenge/Desktop/francis/Scenic/tests/formats/opendrive/maps/CARLA/Town05.xodr'])
 
             ADEs, FDEs = [], []
             for i in range(6):
-                if self.debug:
-                    p = pd.read_csv(f'{model_path}/results/lanegcn/predictions_{i}_0.csv')
-                    plt.plot(p['X'], p['Y'], color='green')
-
-                # extract predicted trajectories from csv file
-                preds = np.genfromtxt(f'{model_path}/results/lanegcn/predictions_{i}_0.csv', delimiter=',', skip_header=1)
+                # Extract predicted trajectories from CSV file
+                preds = np.genfromtxt(f'{model_path}/results/lanegcn/predictions_{i}_{self.worker_num}.csv', delimiter=',', skip_header=1)
                 pred_len = preds.shape[0]
                 if gt_len < pred_len:
                     preds = preds[:gt_len]
 
-                # compute objectives
+                # Compute metrics
                 ADE = float(
                     sum(
                         math.sqrt(
@@ -82,15 +82,19 @@ class ADE_FDE(multi_objective_monitor):
                     (preds[-1, 0] - gts[-1, 0]) ** 2
                     + (preds[-1, 1] - gts[-1, 1]) ** 2
                 )
-
                 ADEs.append(ADE)
                 FDEs.append(FDE)
-                print(f'ADE: {ADE}, FDE: {FDE}')
+
+                if self.debug:
+                    print(f'ADE: {ADE}, FDE: {FDE}')
+                    p = pd.read_csv(f'{model_path}/results/lanegcn/predictions_{i}_0.csv')
+                    plt.plot(p['X'], p['Y'], color='green')
 
             minADE, minFDE = min(ADEs), min(FDEs)
-            rho = (2-minADE, 2-minFDE)
+            rho = (threshADE - minADE, threshFDE - minFDE)
 
-            plt.show()
+            if self.debug:
+                plt.show()
             return rho
 
         super().__init__(specification, priority_graph)
@@ -102,95 +106,63 @@ def announce(message):
     print(m)
     print(border)
 
-def run_experiment(path, parallel=False, multi_objective=False, use_newtonian=False,
-                   sampler_type=None, headless=False):
-    announce(f'RUNNING SCENIC SCRIPT {path}')
-    model = 'scenic.simulators.newtonian.model' if use_newtonian else None
+def run_experiment(scenic_path, model_path, thresholds=None,
+                    sampler_type=None, parallel=False, headless=False,
+                    output_dir='outputs', debug=False):
+    announce(f'RUNNING SCENIC PROGRAM {scenic_path}')
+    
     params = {'verifaiSamplerType': sampler_type} if sampler_type else {}
     params['render'] = not headless
-    if use_newtonian:
-        params['model'] = model
-    sampler = ScenicSampler.fromScenario(path, **params)
+    sampler = ScenicSampler.fromScenario(scenic_path, **params)
     falsifier_params = DotMap(
-        n_iters=None,
+        n_iters=10,
         save_error_table=True,
         save_safe_table=True,
-        max_time=30,
+        max_time=None,
     )
     server_options = DotMap(maxSteps=100, verbosity=0)
-    monitor = ADE_FDE() if multi_objective
+    monitor = ADE_FDE(model_path, thresholds=thresholds, worker_num=0, debug=debug) \
+        if thresholds else \
+        ADE_FDE(model_path, worker_num=0, debug=debug)
 
     falsifier_cls = generic_parallel_falsifier if parallel else generic_falsifier
-    
     falsifier = falsifier_cls(sampler=sampler, falsifier_params=falsifier_params,
-                                  server_class=ScenicServer,
-                                  server_options=server_options,
-                                  monitor=monitor, scenic_path=path, scenario_params=params)
+                                server_class=ScenicServer, server_options=server_options,
+                                monitor=monitor, scenic_path=scenic_path,
+                                scenario_params=params)
     t0 = time.time()
     falsifier.run_falsifier()
     t = time.time() - t0
-    print()
-    print(f'Generated {len(falsifier.samples)} samples in {t} seconds with {falsifier.num_workers} workers')
+    print(f'\nGenerated {len(falsifier.samples)} samples in {t} seconds with {falsifier.num_workers} workers')
     print(f'Number of counterexamples: {len(falsifier.error_table.table)}')
     print(f'Confidence interval: {falsifier.get_confidence_interval()}')
-    return falsifier
+    
+    df = pd.concat([falsifier.error_table.table, falsifier.safe_table.table])
+    root, _ = os.path.splitext(scenic_path)
+    outfile = root.split('/')[-1]
+    if parallel:
+        outfile += '_parallel'
+    if sampler_type:
+        outfile += f'_{sampler_type}'
+    outfile += '.csv'
+    outpath = os.path.join(output_dir, outfile)
+    announce(f'SAVING OUTPUT TO {outpath}')
+    df.to_csv(outpath)
 
-def run_experiments(path, parallel=False, multi_objective=False, use_newtonian=False,
-					sampler_type=None, headless=False, output_dir='outputs'):
-	if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-    paths = []
-    if os.path.isdir(path):
-        for root, _, files in os.walk(path):
-            for name in files:
-                fname = os.path.join(root, name)
-                if os.path.splitext(fname)[1] == '.scenic':
-                    paths.append(fname)
-    else:
-        paths = [path]
-    for p in paths:
-        falsifier = run_experiment(p, parallel=parallel, multi_objective=multi_objective,
-        use_newtonian=use_newtonian, sampler_type=sampler_type, headless=headless)
-        df = pd.concat([falsifier.error_table.table, falsifier.safe_table.table])
-        root, _ = os.path.splitext(p)
-        outfile = root.split('/')[-1]
-        if parallel:
-            outfile += '_parallel'
-        if multi_objective:
-            outfile += '_multi'
-        if use_newtonian:
-            outfile += '_newton'
-        if sampler_type:
-            outfile += f'_{sampler_type}'
-        outfile += '.csv'
-        outpath = os.path.join(output_dir, outfile)
-        announce(f'SAVING OUTPUT TO {outpath}')
-        df.to_csv(outpath)
+    return falsifier
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', '-p', type=str, default='/home/carla_challenge/Desktop/francis/Scenic/examples/carla/Behavior_Prediction/intersection/intersection_01.scenic', help='Path to Scenic script')
-    parser.add_argument('--parallel', action='store_true')
+    parser.add_argument('--path', '-p', type=str, default='/home/carla_challenge/Desktop/francis/Scenic/examples/carla/Behavior_Prediction/intersection/intersection_01.scenic', help='Path to Scenic program')
     parser.add_argument('--model', '-m', type=str, default='/home/carla_challenge/Desktop/francis/LaneGCN', help='Path to behavior prediction model')
+    parser.add_argument('--threshold', '-t', type=float, action='append')
+    parser.add_argument('--samplerType', '-s', type=str, default=None,
+    help='verifaiSamplerType to use')
+    parser.add_argument('--parallel', action='store_true')
+    parser.add_argument('--headless', action='store_true')
     parser.add_argument('--debug', action='store_true')
     args = parser.parse_args()
 
-    monitor = ADE_FDE(args.model, debug=args.debug)
-    falsifier = run_experiment(args.path, monitor, args.parallel)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', '-p', type=str, default='/home/carla_challenge/Desktop/francis/Scenic/examples/carla/Behavior_Prediction/intersection',
-    help='Path to Scenic script or directory of Scenic scripts')
-    parser.add_argument('--parallel', action='store_true')
-    parser.add_argument('--num-workers', type=int, default=5, help='Number of parallel workers')
-    parser.add_argument('--sampler-type', '-s', type=str, default=None,
-    help='verifaiSamplerType to use')
-    parser.add_argument('--multi-objective', action='store_true')
-    parser.add_argument('--newtonian', '-n', action='store_true')
-    parser.add_argument('--headless', action='store_true')
-    args = parser.parse_args()
-
-    run_experiments(args.path, args.parallel, args.multi_objective,
-    	use_newtonian=args.newtonian, sampler_type=args.sampler_type, headless=args.headless)
-
+    falsifier = run_experiment(args.path, args.model,
+        thresholds=args.thresholds, sampler_type=args.samplerType,
+        parallel=args.parallel, headless=args.headless, debug=args.debug)
